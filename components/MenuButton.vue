96ed34e46bffe148c974d8b7ffe78159500dc10f
Jake <inbox@jakekapitz.com>
2019-08-15 07:20:15 +1000

    Menu Button div wrap

--- components/MenuButton.vue
+++ components/MenuButton.vue
@@ -4,7 +4,9 @@
       class="yellow-green md:w-16 md:h-16 lg:w-32 lg:h-32 mx-4 text-white rounded-lg shadow-lg hover:shadow-xl transition-shadow"
       @click.prevent="$store.commit('toggleDisplay')"
     >
-      <fa :icon="['fal', 'bars']" size="4x" />
+      <span class="md:w-8 md:h-8 lg:w-16 lg:h-16 text-white">
+        <fa :icon="['fal', 'bars']" />
+      </span>
     </button>
   </div>
 </template>
